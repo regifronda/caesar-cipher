ef156af987a395d56f8802b45278c15295b35da7
Regi Fronda <regisfronda@gmail.com>
2021-06-14 01:18:32 -0700

    Duplicate variable alphabet and assign to_alphabet

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 alphabet = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"
 
 def caesar_cipher(string, key)
-  string = string.to_i
-  puts string + 6
+  dup_alphabet = alphabet.dup
+  to_alphabet = dup_alphabet + dup_alphabet.slice(0, key * 2) 
 end
 
 caesar_cipher("4", 5)
