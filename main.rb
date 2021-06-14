35c43550222cc87f85a07d0794ec52d36368a4f8
Regi Fronda <regisfronda@gmail.com>
2021-06-14 16:08:21 -0700

    Add ! to slice

--- main.rb
+++ main.rb
@@ -1,8 +1,7 @@
-alphabet = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"
+Alphabet = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"
 
 def caesar_cipher(string, key)
-  dup_alphabet = alphabet.dup
-  to_alphabet = dup_alphabet + dup_alphabet.slice(0, key * 2) 
+  dup_alphabet = Alphabet.dup
+  to_alphabet = dup_alphabet + dup_alphabet.slice!(0, key * 2) 
+  string.tr(Alphabet, to_alphabet)
 end
-
-caesar_cipher("4", 5)
