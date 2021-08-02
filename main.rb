ed3cf725c26dc53e6ed943d97e44fab35da3b857
Regi Fronda <regisfronda@gmail.com>
2021-08-02 14:21:09 -0700

    Add CaesarCipher class

--- main.rb
+++ main.rb
@@ -1,7 +1,12 @@
-Alphabet = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"
+class CaesarCipher
+  Alphabet = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"
 
-def caesar_cipher(string, key)
-  dup_alphabet = Alphabet.dup
-  to_alphabet = dup_alphabet + dup_alphabet.slice!(0, key * 2) 
-  string.tr(Alphabet, to_alphabet)
+  def caesar_cipher(string, key)
+    dup_alphabet = Alphabet.dup
+    to_alphabet = dup_alphabet + dup_alphabet.slice!(0, key * 2) 
+    string.tr(Alphabet, to_alphabet)
+  end
 end
+
+p caesar_cipher("What a string!", 5)
+p caesar_cipher("ZzZ!", 5)
