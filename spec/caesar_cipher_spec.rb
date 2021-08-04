4722cc4b0e26e04ac7fc9c2a180982019ecb0ecc
Regi Fronda <regisfronda@gmail.com>
2021-08-04 00:26:22 -0700

    Add test for large right shift

---
+++ spec/caesar_cipher_spec.rb
@@ -0,0 +1,20 @@
+require './lib/main.rb'
+
+describe CaesarCipher do
+  describe "#caesar_cipher" do
+    it "returns letters replaced by a right shift" do
+      cipher = CaesarCipher.new
+      expect(cipher.caesar_cipher("What a string!", 5)).to eql("Bmfy f xywnsl!")
+    end
+
+    it "returns letters replaced by a left shift" do
+      cipher = CaesarCipher.new
+      expect(cipher.caesar_cipher("What a string!", -5)).to eql("Rcvo v nomdib!")
+    end
+
+    it "returns a string replaced by a large right shift" do
+      cipher = CaesarCipher.new
+      expect(cipher.caesar_cipher("Zebra!", 101)).to eql("Wbyox!")
+    end
+  end
+end
