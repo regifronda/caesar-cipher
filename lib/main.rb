c4dfe657a8009790a4a47fc21d92cd26e37bc43e
Regi Fronda <regisfronda@gmail.com>
2021-08-04 00:32:55 -0700

    Remove p statements

--- lib/main.rb
+++ lib/main.rb
@@ -1,13 +1,8 @@
 class CaesarCipher
 
   def caesar_cipher(string, key = 13)
-    mapping = Hash.new{ |_, y| y }.merge(Hash[['A'..'Z', 'a'..'z'].
+    mapping = Hash.new{ |_, y| y }.merge(Hash[["A".."Z", "a".."z"].
       flat_map { |x| x.zip(x.to_a.rotate key)}])
     string.each_char.map{ |x| mapping[x] }.join
   end
 end
-
-c = CaesarCipher.new
-p c.caesar_cipher("What a string!", 5)
-p c.caesar_cipher("ZzZ!", 5)
-#p c.caesar_cipher("String", -1)
